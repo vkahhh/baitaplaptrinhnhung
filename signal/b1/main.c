588f15dd2d5f26251daa01fce4ccedc6a7096c9a
vkahhh <7.luhan.m.luexolu@gmail.com>
2021-12-25 20:50:14 -0800

    oi

---
+++ signal/b1/main.c
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <signal.h>
+static void signal_handler1(int signo) {
+    
+    if (signo == SIGINT) {
+        printf("OK.\n");
+  }
+}
+
+int main(void) {
+    signal (SIGINT, signal_handler1);
+    while (1);
+    if (signal(SIGINT, signal_handler1) == SIG_ERR) {
+        printf("error.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+   return 0;
+   }
---
+++ signal/b2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void signal_Handler1(int signo) {
+  if (signo == SIGUSR1) {
+    printf("SIGUSR1 signal.\n");
+  }
+}
+void signal_Handler2(int signo) {
+  if (signo == SIGUSR2) {
+    printf("SIGUSR2 signal.\n");
+  }
+}
+
+int main(void) {
+    signal(SIGUSR1, signal_Handler1);
+    signal(SIGUSR2, signal_Handler2);
+    
+    sigset_t new_set, old_set;
+    sigemptyset( &new_set);
+    sigemptyset( &old_set);
+    
+    sigaddset( &new_set, SIGINT );
+    sigprocmask( SIG_BLOCK, &new_set, &old_set);
+    sigprocmask( SIG_SETMASK, &old_set, NULL );
+    if (sigismember (newset, SIGINT) == 1) { //đang bị block
+        sigprocmask( SIG_UNBLOCK, &new_set, &old_set);
+    } else {
+        sigprocmask( SIG_BLOCK, &new_set, &old_set);
+    }
+
+    while (1);
+
+   return 0;
+   }
+
+
+
